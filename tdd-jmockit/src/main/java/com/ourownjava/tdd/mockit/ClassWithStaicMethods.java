a55ffbf9c3aba68656b1e6d176ed00ce0fbab13a
sanjuthomas <sanju@sanju.org>
2014-08-10 14:25:06 -0400

    testing static methods

---
+++ tdd-jmockit/src/main/java/com/ourownjava/tdd/mockit/ClassWithStaicMethods.java
@@ -0,0 +1,29 @@
+package com.ourownjava.tdd.mockit;
+
+import java.util.List;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class ClassWithStaicMethods {
+	
+	private static void privateStaticBehavior(){
+		throw new RuntimeException("I am a private static method. Why did you call me?");
+	}
+	
+	public static void publicStaticBehavior(){
+		throw new RuntimeException("I am a public static method. Why did you call me?");
+	}
+	
+	private static void privateStaticBehaviorWithArguments(final List<String> tokens){
+		for(final char c : "Sanju Thomas".toCharArray()){
+			tokens.add(String.valueOf(c));
+		}
+	}
+	
+	private static String [] privateStaticBehaviorWithArgumentsAndReturnValue(final String token, final String delimiter){
+		return token.split(delimiter);
+	}
+}
--- tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/TestClassWithPrivateStaticMethod.java
+++ tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/TestClassWithPrivateStaticMethod.java
@@ -1,10 +1,15 @@
 package com.ourownjava.tdd.jmockit;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import mockit.Deencapsulation;
 
 import org.junit.Test;
 
-import com.ourownjava.tdd.mockit.ClassWithPrivateStaicMethod;
+import com.ourownjava.tdd.mockit.ClassWithStaicMethods;
 
 /**
  * 
@@ -14,8 +19,28 @@ import com.ourownjava.tdd.mockit.ClassWithPrivateStaicMethod;
 public class TestClassWithPrivateStaticMethod {
 
 	@Test(expected = RuntimeException.class)
-	public void shouldInvokePriateStaticMethod(){
-		Deencapsulation.invoke(ClassWithPrivateStaicMethod.class, "staticBehavior", new Object[] {});
+	public void shouldInvokePrivateStaticMethod(){
+		Deencapsulation.invoke(ClassWithStaicMethods.class, "privateStaticBehavior", new Object[] {});
 	}
 	
+	@Test(expected = RuntimeException.class)
+	public void shouldInvokePublicStaticMethod(){
+		Deencapsulation.invoke(ClassWithStaicMethods.class, "publicStaticBehavior", new Object[] {});
+	}
+	
+	@Test
+	public void shouldInvokePrivateStaticMethodWithArguments(){
+		final List<String> tokens = new ArrayList<String>();
+		Deencapsulation.invoke(ClassWithStaicMethods.class, "privateStaticBehaviorWithArguments", tokens);
+		assertEquals(12, tokens.size());
+	}
+	
+	@Test
+	public void shouldInvokePrivateStaticMethodWithArgumentsAndReturnValue(){
+		final String [] tokens = Deencapsulation.invoke(ClassWithStaicMethods.class, 
+				"privateStaticBehaviorWithArgumentsAndReturnValue", new Object[] {"Sanju Thomas", " "});
+		assertEquals(2, tokens.length);
+		assertEquals("Sanju", tokens[0]);
+		assertEquals("Thomas", tokens[1]);
+	}
 }
