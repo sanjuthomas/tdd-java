48e10b952fbaff3702478ea23bab722c45a954b4
sanjuthomas <sanju@sanju.org>
2014-08-12 22:34:23 -0400

    adding static mocks

--- tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/TestYellingManager.java
+++ tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/TestYellingManager.java
@@ -1,22 +1,34 @@
 package com.ourownjava.tdd.jmockit;
 
+import static org.junit.Assert.assertEquals;
 import mockit.Mock;
 import mockit.MockUp;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import com.ourownjava.tdd.mockit.UglyWordUtility;
+import com.ourownjava.tdd.mockit.YellingManager;
 
 public class TestYellingManager {
-	
-	 public void shouldYell(){
-			new MockUp<UglyWordUtility>()
-			{
-				@Mock
-				public String isEventDispatchThread()
-				{
-					return "Your are a real nice guy"; 
-				}
-			};
-				
-			
-	 }
+
+	private YellingManager yellingManager;
+
+	@Before
+	public void setUp() {
+		yellingManager = new YellingManager();
+	}
+
+	@Test
+	public void shouldYell() {
+		new MockUp<UglyWordUtility>() {
+			@Mock
+			public String getUglyword() {
+				return "Your are a real nice guy";
+			}
+		};
+
+		final String yell = yellingManager.yell();
+		assertEquals("Your are a real nice guy", yell);
+	}
 }
