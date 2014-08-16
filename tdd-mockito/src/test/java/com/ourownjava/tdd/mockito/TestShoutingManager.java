6c8591488f8a56725beff1fb0e5df23aae9ca2fe
sanjuthomas <sanju@sanju.org>
2014-08-16 12:35:17 -0400

    mockito driven test cases

--- tdd-mockito/src/test/java/com/ourownjava/tdd/mockito/TestShoutingManager.java
+++ tdd-mockito/src/test/java/com/ourownjava/tdd/mockito/TestShoutingManager.java
@@ -1,16 +1,25 @@
 package com.ourownjava.tdd.mockito;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 /**
  * 
  * @author Sanju Thomas
  *
  */
+@RunWith(MockitoJUnitRunner.class)
 public class TestShoutingManager {
    
+	@InjectMocks
 	private ShoutingManager shoutingManager;
 	
 	@Mock
@@ -18,12 +27,14 @@ public class TestShoutingManager {
 	
 	@Before
 	public void setUp(){
-		this.shoutingManager = new ShoutingManager();
+		initMocks(this);
+		when(dirtyWordService.getADirtyWord()).thenReturn("You are such a nice persion");
 	}
 	
 	@Test
 	public void shouldShout(){
-		
+		final String shout = shoutingManager.shout();
+		assertEquals("You are such a nice persion", shout);
 	}
 	
 }
