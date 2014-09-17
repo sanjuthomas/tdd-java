eeff269949cd2c1f84e9c96eca479e8a5033fc44
Sanju Thomas <sanju@sanju.org>
2014-09-17 10:37:16 -0400

    TestAuthenticationManager
    TestAuthenticationManager

---
+++ tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/TestAuthenticationManager.java
@@ -0,0 +1,51 @@
+package com.ourownjava.tdd.mockit;
+import static org.junit.Assert.assertEquals;
+
+import javax.naming.AuthenticationException;
+
+import mockit.Deencapsulation;
+import mockit.Injectable;
+import mockit.NonStrictExpectations;
+import mockit.Verifications;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class TestAuthenticationManager {
+	
+	private AuthenticationManager authenticationManager;
+	
+	@Injectable
+	private ThridPartyAuthenticationService thridPartyAuthenticationService;
+	
+	@Before
+	public void setUp(){
+		authenticationManager = new AuthenticationManager();
+		Deencapsulation.setField(authenticationManager, "authenticationService", thridPartyAuthenticationService);
+	}
+	
+	@Test
+	public void shouldNotAuthenticate() throws AuthenticationException{
+		
+		new NonStrictExpectations() {{
+			thridPartyAuthenticationService.authenticate((Principal) any);
+			returns(true);
+		}};
+		
+		final Principal pricipal = new Principal("thegoodguy");
+		authenticationManager.authenticate(pricipal);
+		
+		new Verifications() {{
+			Principal vPrincipal;
+			thridPartyAuthenticationService.authenticate(vPrincipal = withCapture());
+			assertEquals("thegoodguy", vPrincipal.getLoginId());
+		}};
+	}
+	
+}
