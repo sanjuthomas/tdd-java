604205d39232d7b263030b28aeda2618f1de4c62
Sanju Thomas <sanju@sanju.org>
2014-09-17 10:34:34 -0400

    ThridPartyAuthenticationService.java
    ThridPartyAuthenticationService.java

---
+++ tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/ThridPartyAuthenticationService.java
@@ -0,0 +1,20 @@
+package com.ourownjava.tdd.mockit;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class ThridPartyAuthenticationService {
+
+	/**
+	 * Not yet implemented. 
+	 * 
+	 * @param pricipal
+	 * @return
+	 */
+	public boolean authenticate(Principal pricipal) {
+		return false;
+	}
+
+}
