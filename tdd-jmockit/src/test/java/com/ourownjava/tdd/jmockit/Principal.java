59a3a0a71d80dd7867c1cac38db2b980389ac58c
Sanju Thomas <sanju@sanju.org>
2014-09-17 10:34:15 -0400

    Principal
    Principal

---
+++ tdd-jmockit/src/test/java/com/ourownjava/tdd/jmockit/Principal.java
@@ -0,0 +1,21 @@
+package com.ourownjava.tdd.mockit;
+
+/**
+ * 
+ * @author Sanju Thomas
+ *
+ */
+public class Principal {
+	
+	public Principal(final String loginId){
+		this.loginId = loginId;
+	}
+	
+	private String loginId;
+
+	public String getLoginId() {
+		return loginId;
+	}
+
+	
+}
